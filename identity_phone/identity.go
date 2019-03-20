75d7a2ea5106d5a6d8651ac278d8e92dc35b5731
Mikhail Reshetnyak <micresh@gmail.com>
2019-03-20 08:51:04 +0300

    realise methodos for phonenumber identity

--- identity_phone/identity.go
+++ identity_phone/identity.go
@@ -1,20 +1,31 @@
 package identity_phone
 
-import "github.com/themakers/identity/identity"
+import (
+	"github.com/themakers/identity/identity"
+	"unicode"
+)
 
 var _ identity.Identity = new(Identity)
 
 type Identity struct {
-
 }
 
 func (idn *Identity) Info() identity.IdentityData {
 
-	return identity.IdentityData{}
+	return identity.IdentityData{Name: "Phone"}
 }
 
-func (idn *Identity) NormalizeData(identity string)  string{
-
-	return identity
+func (idn *Identity) NormalizeData(identity string) (result string) {
+	for _, c := range identity {
+		if unicode.IsDigit(c) {
+			result += string(rune(c))
+		}
+	}
+	if len(result) == 11 && result[0] == '8' {
+		result = string(rune('7')) + result[1:]
+	} else if len(result) == 10 && result[0] == '9' {
+		result = string(rune('7')) + result[:]
+	}
+	return
 
 }

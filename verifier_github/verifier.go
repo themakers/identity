59b0ee835ee48fe073f9d5555c8e7cd58c375567
skeris <kotilion.95@gmail.com>
2019-04-12 12:09:26 +0300

    fix referens argument UserInfo ror unmarshal

--- verifier_github/verifier.go
+++ verifier_github/verifier.go
@@ -94,7 +94,7 @@ func (prov *Verifier) GetOAuth2Identity(ctx context.Context, accessToken string)
 
 	var user UserInfo
 	//todo: validate service answer
-	if err := json.Unmarshal(data, user); err != nil {
+	if err := json.Unmarshal(data, &user); err != nil {
 		return nil, err
 	}
 

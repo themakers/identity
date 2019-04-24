b0812028ef096ab24c43f31dc5802fb5d9ecb018
skeris <kotilion.95@gmail.com>
2019-04-24 13:50:18 +0300

    change bcrypt to scrypt in statis verification

--- verifier_password/verifier.go
+++ verifier_password/verifier.go
@@ -3,8 +3,10 @@ package verifier_password
 import (
 	"context"
 	"github.com/themakers/identity/identity"
-	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/scrypt"
 )
+
+const salt = "salt"
 
 var _ identity.Verifier = new(Verifier)
 
@@ -23,5 +25,9 @@ func New() *Verifier {
 }
 
 func (ver *Verifier) StartStaticVerification(ctx context.Context, password_hash, password, login string) (err bool) {
-	return bcrypt.CompareHashAndPassword([]byte(password_hash), []byte(password)) == nil
+	hash, hash_err := scrypt.Key([]byte(password), []byte(salt), 1<<15, 8, 1, 32)
+	if hash_err != nil {
+		panic(hash_err)
+	}
+	return string(hash) == password_hash
 }

e0dd826804684d48351060a692614e2d1ebbf324
The Architect <thearchitect@themake.rs>
2019-05-31 17:30:56 +0300

    proto

--- identity/backend.go
+++ identity/backend.go
@@ -3,27 +3,14 @@ package identity
 import "context"
 
 type Backend interface {
-	//-------------------Create section-----------------------------------------
 	GetAuthentication(ctx context.Context, id string) (*Authentication, error)
 	CreateAuthentication(ctx context.Context, id string, objective AuthenticationObjective, userID string) (*Authentication, error)
 	SaveAuthentication(ctx context.Context, auth *Authentication) (*Authentication, error)
 
-	//------------------Get section---------------------------------------------
-	//CreateUser(identity *IdentityData, data *VerifierData) (*User, error)
 	GetUser(ctx context.Context, id string) (*User, error)
 	CreateUser(ctx context.Context, user *User) (*User, error)
 	SaveUser(ctx context.Context, user *User) (*User, error)
-	//GetUserByLogin(login, vername string) (*User, error)
 	GetUserByIdentity(ctx context.Context, identityName, identity string) (*User, error)
-
-	//------------------Add section ------------------------------------------------
-	//AddUserIdentity(uid string, identity *IdentityData) (*User, error)
-	//AddUserAuthenticationData(uid string, data *VerifierData) (*User, error)
-	//AddUserToAuthentication(aid, uid string) (*Authentication, error)
-	//AddTempAuthDataToAuth(aid string, data map[string]map[string]string) (*Authentication, error)
-	//AddUserVerifier(uid string, data *VerifierData) (*User, error)
-	//UpdateFactorStatus(aid, VerifierName string) error
-	//DropIdentity(identity *IdentityData) error
 }
 
 type User struct {
